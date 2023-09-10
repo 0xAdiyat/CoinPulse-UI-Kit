6919d8f4029d2b60667c5b91f917767c9b7055d3
0xadiyat <0xAdiyat@gmail.com>
2023-09-10 23:37:05 +0600

    Improved structure added custom icons

---
+++ coin_pulse/lib/view/deposit_page/deposit_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DepositPage extends StatelessWidget {
+  const DepositPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
